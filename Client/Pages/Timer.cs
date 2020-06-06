249bee0a3ba9a1877128ddf921afabbda3ce9fb8
Wojtek Henszke <wojciech.henszke@nokia.com>
2020-06-06 21:15:37 +0200

    Parent-Child comunication

---
+++ Client/Pages/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore.Components;
+
+namespace MyFirstBlazor.Client.Pages
+{
+    public class Timer : ComponentBase
+    {
+        [Parameter]
+        public double TimeInSeconds { get; set; }
+        [Parameter]
+        public Action Tick { get; set; }
+
+        protected override void OnInitialized()
+        {
+            var timer = new System.Threading.Timer(
+                (_)=> InvokeAsync(()=>Tick?.Invoke()),
+                null,
+                TimeSpan.FromSeconds(TimeInSeconds),
+                Timeout.InfiniteTimeSpan );
+        }
+    }
+}
